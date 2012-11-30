ded43013cdcace44f87fd12cb24a4be1debd76b3
Joram Barrez <joram.barrez@gmail.com>
2012-11-30 11:55:42 +0100

    First commit

---
+++ run-upgrade-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export MAVEN_OPTS="-Xms521M -Xmx1024M"
+if [[ "$3" == "" ]] ; then
+
+   	echo "Missing argument: correct usage <./run-upgrade-test.sh oldVersion newVersion database>"
+   	
+else
+
+	OLD_VERSION_MODULE="activiti-upgrade-$1"
+    echo "Old version module: $OLD_VERSION_MODULE"
+	NEW_VERSION_MODULE="activiti-upgrade-$2"
+    echo "Old version module: $NEW_VERSION_MODULE"
+    DATABASE=$3
+    echo "Database type: $DATABASE"
+        
+	echo
+    echo "Running old version module"
+    echo
+    cd $OLD_VERSION_MODULE
+    mvn -Ddatabase=$DATABASE -DoldVersion=true -Dmaven.test.skip=true clean test
+    
+    STATUS=$?
+	if [ $STATUS -eq 0 ] 
+	then
+		echo
+		echo "Running new version module"
+		echo
+    	cd ..
+    	cd $NEW_VERSION_MODULE
+    	mvn -Ddatabase=$DATABASE clean test
+	else
+		echo
+		echo
+    	echo "Build failure on old version module. Halting."
+    	echo
+	fi   
+	 
+fi
