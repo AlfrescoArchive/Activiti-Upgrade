1168817fd1a6a149efe16a4199530fcd79ed1369
Joram Barrez <joram.barrez@gmail.com>
2014-03-24 14:21:03 +0100

    - Additional 5.15 tests - Added 5.16-SNAPSHOT

--- activiti-upgrade-5.15/src/main/java/org/activiti/upgrade/DataGenerator.java
+++ activiti-upgrade-5.15/src/main/java/org/activiti/upgrade/DataGenerator.java
@@ -1,10 +1,16 @@
 package org.activiti.upgrade;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.persistence.entity.JobEntity;
+import org.activiti.engine.impl.persistence.entity.JobEntityManager;
+import org.activiti.engine.runtime.Job;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.upgrade.helper.UpgradeUtil;
 
@@ -29,6 +35,11 @@ public class DataGenerator {
   public static void main(String[] args) {
     ProcessEngine processEngine = UpgradeUtil.getProcessEngine();
     createCommonData(processEngine);
+    
+    // 5.15 specific
+    createTaskCategoryData(processEngine);
+    createHistoricVariableTimeStoreTestData(processEngine);
+    createTimeStampPrecisionUpgradeData(processEngine);
   }
   
   private static void createCommonData(ProcessEngine processEngine) {
@@ -55,6 +66,56 @@ public class DataGenerator {
     variables.put("instrument", "trumpet");
     variables.put("player", "gonzo");
     runtimeService.startProcessInstanceByKey("taskWithExecutionVariablesProcess", variables);
+  }
+  
+  private static void createTaskCategoryData(ProcessEngine processEngine) {
+  	 RuntimeService runtimeService = processEngine.getRuntimeService();
+     TaskService taskService = processEngine.getTaskService();
+
+     processEngine.getRepositoryService()
+       .createDeployment()
+       .name("simpleTaskProcess")
+       .addClasspathResource("org/activiti/upgrade/test/testTaskCategory.bpmn20.xml")
+       .deploy();
+
+     // Process instance is started and user task is available
+     // The 5.15 upgrade will add the category later after upgrade
+     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("testTaskCategory");
+  }
+  
+  private static void createHistoricVariableTimeStoreTestData(ProcessEngine processEngine) {
+  	 processEngine.getRepositoryService().createDeployment().name("simpleTaskProcess")
+     	.addClasspathResource("org/activiti/upgrade/test/HistoricVariableTimeStoreTest.bpmn20.xml")
+     	.deploy();
+  	 
+  	 HashMap<String, Object> vars = new HashMap<String, Object>();
+  	 vars.put("myVar", "myValue");
+  	 processEngine.getRuntimeService().startProcessInstanceByKey("historicVariableTimeStoreTestProcess", vars);
+  }
+  
+  private static void createTimeStampPrecisionUpgradeData(ProcessEngine processEngine) {
+	  processEngine.getRepositoryService().createDeployment().name("simpleTaskProcess")
+   		.addClasspathResource("org/activiti/upgrade/test/testTimestampPrecisionUpgrade.bpmn20.xml")
+   		.deploy();
+	  
+	  // Start three process instances for this process
+	  ProcessInstance processInstance = null;
+	  for (int i=0; i<3; i++) {
+		  processInstance = processEngine.getRuntimeService().startProcessInstanceByKey("testTimestampPrecisionUpgrade");
+	  }
+	  
+	  // Set a lock time for the job (hacky)
+	  final Job job = processEngine.getManagementService().createJobQuery().processInstanceId(processInstance.getId()).singleResult();
+	  processEngine.getManagementService().executeCommand(new Command<Void>() {
+		public Void execute(CommandContext commandContext) {
+			JobEntityManager jobEntityManager = commandContext.getJobEntityManager();
+			JobEntity jobEntity = jobEntityManager.findJobById(job.getId());
+			jobEntity.setLockExpirationTime(new Date());
+			jobEntity.setLockOwner("Upgrade test");
+			jobEntity.setDuedate(new Date());
+			return null;
+		}
+	  });
   }
 
 }
---
+++ activiti-upgrade-5.15/src/test/java/org/activiti/upgrade/test/TimestampUpgradePrecisionTest.java
@@ -0,0 +1,68 @@
+package org.activiti.upgrade.test;
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.List;
+
+import org.activiti.engine.impl.persistence.entity.JobEntity;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.Job;
+import org.activiti.upgrade.test.helper.RunOnlyWithTestDataFromVersion;
+import org.activiti.upgrade.test.helper.UpgradeTestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * This is an upgrade test added for the 5.15 release. In that release, 
+ * we've upgraded the timestamp columns for mysql for ms precision.
+ * 
+ * @author Joram Barrez
+ */
+@RunOnlyWithTestDataFromVersion(versions = {"5.14"})
+public class TimestampUpgradePrecisionTest extends UpgradeTestCase {
+
+  @Test
+  public void testCreateAndLastUpdatedTimeStamp() {
+	  
+	  // Find process definition
+	  ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+			  .processDefinitionKey("testTimestampPrecisionUpgrade").singleResult();
+	  
+	  // After upgrade, there should be three jobs availble.
+	  // One has a duedate set and one has a lock time set
+	  
+	  List<Job> jobs = managementService.createJobQuery().processDefinitionId(processDefinition.getId()).list();
+	  Assert.assertEquals(3, jobs.size());
+	  boolean oneHasDuedate = false;
+	  boolean oneHasLockDate = false;
+	  for (Job job : jobs) {
+		  
+		  if (job.getDuedate() != null && !oneHasDuedate) {
+			  oneHasDuedate = true;
+		  } else if (job.getDuedate() != null && oneHasDuedate) {
+			  Assert.fail("Only one with due date is expected");
+		  }
+		  
+		  JobEntity jobEntity = (JobEntity) job;
+		  if (jobEntity.getLockExpirationTime() != null && !oneHasLockDate) {
+			  oneHasLockDate = true;
+		  } else if (jobEntity.getLockExpirationTime() != null && oneHasLockDate) {
+			  Assert.fail("Obnly one with a lock is expected");
+		  }
+	  }
+	  
+	  Assert.assertTrue("One job should have due date, but none found", oneHasDuedate);
+	  Assert.assertTrue("One job should have lock expiration date, but none found", oneHasLockDate);
+  }
+
+}
